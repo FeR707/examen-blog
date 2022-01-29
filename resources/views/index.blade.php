0945b7bfb61f6b1c97a9b1a2a9a14bc389324cab
Your Name <you@example.com>
2022-01-28 22:50:27 -0600

    s

--- resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('template.layout')
-
-@section('inicio')
-    <h1>Bienvenidos a nuestro blog
-    </h1>
-    <p>Aqui encontarras informacion util relacionado con Cliente - Servidor</p>
-@endsection
